74f54578d6c47d209d91ace82fbbbaf612dfecd9
Chetna Gupta <66460002+chetu26@users.noreply.github.com>
2022-12-30 13:38:23 +0530

    Add files via upload

---
+++ EditWordFiles/WordFileModificationForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace EditWordFiles
+{
+    partial class WordFileModificationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browsePdfBtn = new System.Windows.Forms.Button();
+            this.pdfTextBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.docTextBox = new System.Windows.Forms.TextBox();
+            this.modifyBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // browsePdfBtn
+            // 
+            this.browsePdfBtn.Location = new System.Drawing.Point(35, 33);
+            this.browsePdfBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.browsePdfBtn.Name = "browsePdfBtn";
+            this.browsePdfBtn.Size = new System.Drawing.Size(165, 33);
+            this.browsePdfBtn.TabIndex = 0;
+            this.browsePdfBtn.Text = "Select PDF Folder";
+            this.browsePdfBtn.UseVisualStyleBackColor = true;
+            this.browsePdfBtn.Click += new System.EventHandler(this.BrowsePdfBtn_Click);
+            // 
+            // pdfTextBox
+            // 
+            this.pdfTextBox.Location = new System.Drawing.Point(232, 33);
+            this.pdfTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pdfTextBox.Multiline = true;
+            this.pdfTextBox.Name = "pdfTextBox";
+            this.pdfTextBox.Size = new System.Drawing.Size(439, 32);
+            this.pdfTextBox.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(35, 106);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 33);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Select Template File";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.BrowseDocBtn_Click);
+            // 
+            // docTextBox
+            // 
+            this.docTextBox.Location = new System.Drawing.Point(232, 106);
+            this.docTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.docTextBox.Multiline = true;
+            this.docTextBox.Name = "docTextBox";
+            this.docTextBox.Size = new System.Drawing.Size(439, 32);
+            this.docTextBox.TabIndex = 4;
+            // 
+            // modifyBtn
+            // 
+            this.modifyBtn.Location = new System.Drawing.Point(507, 171);
+            this.modifyBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.modifyBtn.Name = "modifyBtn";
+            this.modifyBtn.Size = new System.Drawing.Size(165, 33);
+            this.modifyBtn.TabIndex = 5;
+            this.modifyBtn.Text = "Transform";
+            this.modifyBtn.UseVisualStyleBackColor = true;
+            this.modifyBtn.Click += new System.EventHandler(this.ModifyBtn_Click);
+            // 
+            // WordFileModificationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 266);
+            this.Controls.Add(this.modifyBtn);
+            this.Controls.Add(this.docTextBox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pdfTextBox);
+            this.Controls.Add(this.browsePdfBtn);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "WordFileModificationForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDF TO Word Processor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browsePdfBtn;
+        private System.Windows.Forms.TextBox pdfTextBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox docTextBox;
+        private System.Windows.Forms.Button modifyBtn;
+    }
+}
+
---
+++ EditWordFiles/WordFileModificationForm.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using Microsoft.Office.Interop.Word;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Table = DocumentFormat.OpenXml.Wordprocessing.Table;
+using DataTable = System.Data.DataTable;
+using Paragraph = DocumentFormat.OpenXml.Wordprocessing.Paragraph;
+using Column = DocumentFormat.OpenXml.Wordprocessing.Column;
+using DocumentFormat.OpenXml;
+
+namespace EditWordFiles
+{
+    public partial class WordFileModificationForm : Form
+    {
+        public WordFileModificationForm()
+        {
+            InitializeComponent();
+        }
+        public string pdfFilePath = null;
+        //public string companyName = null;
+        public int totalAmmountIndex = 0;
+        //double totalAmount = 0;
+        Dictionary<string, double> companyNameAndTotalAmountDict = new Dictionary<string, double>();
+        public string docFilePath = null;
+        public object txtFileContent { get; private set; }
+
+        public void BrowsePdfBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog openFileDialog = new FolderBrowserDialog();
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pdfTextBox.Text = openFileDialog.SelectedPath;
+                    pdfFilePath = pdfTextBox.Text;
+                    GetTextFromPDF();
+                }
+
+                //GetTextFromWord();
+            }
+            catch (Exception ex)
+            {
+                LoggingLibrary.LoggingManager.LogException(this.GetType(), ex);
+                MessageBox.Show("Failure! : " + ex.Message);
+            }
+        }
+
+        private void GetTextFromPDF()
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(pdfFilePath, "*.pdf");
+                if (files.Length < 1)
+                {
+                    throw new Exception("There is no PDF file in this folder");
+                }
+                foreach (string file in files)
+                {
+                    string companyName = null;
+                    double totalAmount = -1;
+                    PdfReader reader = new PdfReader(file);
+                    int PageNum = reader.NumberOfPages;
+                    string[] words;
+                    string text;
+                    //double value = 0;
+
+                    for (int i = 1; i <= PageNum; i++)
+                    {
+                        text = PdfTextExtractor.GetTextFromPage(reader, i, new LocationTextExtractionStrategy()).Trim();
+                        text = Encoding.UTF8.GetString(Encoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(text)));
+                        words = text.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+                        if (i == 1)
+                        {
+                            companyName = words[0].Trim();
+                        }
+                        else if (i == 2)
+                        {
+                            for (int w = 0; w <= words.Length; w++)
+                            {
+                                if (words[w].Trim() == "TOTAL AMOUNT")
+                                {
+                                    totalAmmountIndex = w;
+                                    break;
+                                }
+                            }
+                            for (int t = totalAmmountIndex; t >= 0; t--)
+                            {
+                                if (double.TryParse(words[t].Trim(), out totalAmount))
+                                {
+                                    if (totalAmount > 0)
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if ((!string.IsNullOrEmpty(companyName)) && totalAmount >= 0)
+                    {
+                        companyNameAndTotalAmountDict[companyName] = totalAmount;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                LoggingLibrary.LoggingManager.LogException(this.GetType(), ex);
+                MessageBox.Show("Failure! : " + ex.Message);
+            }
+
+
+        }
+        private void BrowseDocBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Word document (*.doc*)|*.doc*";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    docTextBox.Text = openFileDialog.FileName;
+                }
+                docFilePath = docTextBox.Text;
+            }
+            catch (Exception ex)
+            {
+                LoggingLibrary.LoggingManager.LogException(this.GetType(), ex);
+                MessageBox.Show("Failure! : " + ex.Message);
+            }
+        }
+
+        private void AddColumnAndData(string companyName,double totalAmount)
+        {
+            try
+            {
+                DataSet dataSet = new DataSet();
+                using (var doc = WordprocessingDocument.Open(docTextBox.Text.Trim(), true))
+                {
+                    List<Table> tables = doc.MainDocumentPart.Document.Body.Elements<Table>().Where(tbl => tbl.InnerText.Contains("Joint Evaluation") || tbl.InnerText.Contains("Commercial Evaluation")).ToList();
+                    // To get all rows from table  
+                    for (int i = 0; i < tables.Count; i++)
+                    {
+                        List<TableRow> tableRows = tables[i].Elements<TableRow>().ToList();
+
+                        // Create a cell.
+                        for (int rowId = 0; rowId < tableRows.Count; rowId++)
+                        {
+                            //TableCell tc1 = new TableCell();
+                            TableCell tc1 = (TableCell)tableRows[rowId].Elements<TableCell>().LastOrDefault().CloneNode(true);
+
+                            if (!string.IsNullOrEmpty(tc1.InnerText))
+                                tc1.InnerXml = tc1.InnerXml.Replace(tc1.InnerXml, tc1.InnerXml.Replace(tc1.InnerText, ""));
+
+                            List<Paragraph> p = tc1.Elements<Paragraph>().ToList();
+                            bool isAmountWritten = false;
+                            foreach (var para in p)
+                            {
+                                List<Run> r = para.Elements<Run>().ToList();
+
+                                int runcount = -1;
+                                foreach (var run in r)
+                                {
+                                    runcount++;
+                                    // Set the text for the run.
+                                    Text t = run?.Elements<Text>().FirstOrDefault();
+                                    if (t != null)
+                                    {
+                                        t.Text = "";
+                                    }
+
+                                    //// Specify the table cell content.
+                                    if (rowId == 0)
+                                    {
+                                        TableCellProperties tcp = new TableCellProperties(
+                                        new TableCellWidth { Type = TableWidthUnitValues.Pct, Width = "50%", }
+                                       );
+
+                                        if (t != null && runcount == 0)
+                                            t.Text = companyName;
+
+                                        else if (t == null)
+                                            run.Append(new Text(companyName));                                           
+
+                                        tc1.Append(tcp);
+                                    }
+                                    else if (rowId == 1)
+                                    {
+                                        if (!isAmountWritten)
+                                        {
+                                            if (t != null && runcount == 0)
+                                                t.Text = totalAmount.ToString("#,##0.00");
+                                            else if (t == null || string.IsNullOrEmpty(t.Text))
+                                            {
+                                                run.Append(new Text(totalAmount.ToString("#,##0.00")));                                               
+                                            }
+                                            isAmountWritten = true;
+
+                                        }
+                                    }
+                                    else
+                                    {
+                                        if (t != null && runcount == 0)
+                                            t.Text = "";
+                                        else if (t == null)
+                                            run.Append(new Text(""));                                      
+                                    }
+
+                                   
+                                }
+
+                                if (runcount == -1)
+                                {
+                                    //// Specify the table cell content.
+                                    if (rowId == 0)
+                                    {
+                                        para.Append(new Run(new Text(companyName)));                                       
+                                    }
+                                    else if (rowId == 1)
+                                    {
+                                        if (!isAmountWritten)
+                                        {
+                                            para.Append(new Run(new Text(totalAmount.ToString("#,##0.00"))));
+                                            isAmountWritten = true;
+                                        }
+
+                                    }
+                                    else
+                                    {
+                                        para.Append(new Run(new Text("")));                                       
+                                    }                                   
+                                }
+                            }
+
+                            // Append the table cell to the table row.
+                            tableRows[rowId].Append(tc1);
+                        }
+
+                    }
+
+                    doc.Save();
+                }
+            }
+            catch (Exception ex)
+            {
+                LoggingLibrary.LoggingManager.LogException(this.GetType(), ex);
+                MessageBox.Show("Failure! : " + ex.Message);
+            }
+        }
+
+
+        private void ModifyBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((pdfTextBox.Text == "" || pdfTextBox.Text == null) || (docTextBox.Text == "" || docTextBox.Text == null))
+                {
+                    throw new Exception("Please select PDF folder And Template file");
+                }
+                else if (pdfTextBox.Text == "" || pdfTextBox.Text == null)
+                {
+                    throw new Exception("Please select PDF folder");
+                }
+                else if (docTextBox.Text == "" || docTextBox.Text == null)
+                {
+                    throw new Exception("Please select Template file");
+                }
+                else
+                {
+                    if (FileIsOpen(docFilePath))
+                    {
+                        throw new Exception("Template file is already in use.");
+                    }
+                    foreach (string key in companyNameAndTotalAmountDict.Keys)
+                    {
+                        AddColumnAndData(key, companyNameAndTotalAmountDict[key]);
+                    }
+                    MessageBox.Show("Transformation Successful!");
+                }
+            }
+            catch (Exception ex)
+            {
+                LoggingLibrary.LoggingManager.LogException(this.GetType(), ex);
+                MessageBox.Show("Failure! : " + ex.Message);
+            }
+        }
+
+
+        public bool FileIsOpen(string path)
+        {
+            System.IO.FileStream a = null;
+
+            try
+            {
+                a = System.IO.File.Open(path,
+                System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.None);
+                return false;
+            }
+            catch (System.IO.IOException ex)
+            {
+                LoggingLibrary.LoggingManager.LogException(this.GetType(), ex);
+                return true;
+            }
+
+            finally
+            {
+                if (a != null)
+                {
+                    a.Close();
+                    a.Dispose();
+                }
+            }
+        }
+    }
+}
